f50e6716b313aac44e45495875f1e2f70f439ca9
Raunak Kaushal <145521983+raunakkaushal@users.noreply.github.com>
2024-01-22 08:32:57 +0530

    call_by_value.c

--- C Times/Function/call_by_value.c
+++ C Times/Function/call_by_value.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
-// Function prototype with call by value
 void addTen(int x);
 
 int main() {
     int num = 5;
-
     addTen(num);
-
-    // The original value of 'num' remains unchanged
     printf("Original value of num: %d\n", num);
 
     return 0;
 }
 
-// Function definition with call by value
 void addTen(int x) {
     x = x + 10;
     printf("Inside function, x = %d\n", x);
